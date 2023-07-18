ad12a11e15372fa5373077c64bd7af7eac89d329
mobin <pnamobin@gmail.com>
2023-07-18 23:26:05 +0330

    Play one round of RPC and determine the winner

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -22,13 +22,33 @@ function getComputerChoice(){
 
 function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice){
-        console.log("tie")
+        return "tie";
     }
+    else {
+        if (playerChoice == "rock" && computerChoice == "paper")
+            return "you lose"
+        if (playerChoice == "rock" && computerChoice == "scissors")
+            return "you win"
+        
+        if (playerChoice == "paper" && computerChoice == "scissors")
+            return "you lose"
+        if (playerChoice == "paper" && computerChoice == "rock")
+            return "you win"
+
+        if (playerChoice == "scissors" && computerChoice == "rock")
+            return "you lose"
+        if (playerChoice == "scissors" && computerChoice == "paper")
+            return "you win"
+    }
+    
 }
+
+
 
 computerChoice = getComputerChoice();
 playRound(playerChoice, computerChoice);
 
-console.log(computerChoice)
+console.log(computerChoice);
+console.log(playRound(playerChoice, computerChoice));
 
 
