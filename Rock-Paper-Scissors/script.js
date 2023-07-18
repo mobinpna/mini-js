7e96912dd28df1b4d556264240a94bf096e8228b
mobin <pnamobin@gmail.com>
2023-07-18 20:51:42 +0330

    Make playerChoice case insensitive

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,23 +1,29 @@
 let randomNumber;
-let computerChoice
+let computerChoice;
+let playerChoice = "paper";
+playerChoice = (playerChoice.toLowerCase());
 
 function getComputerChoice(){
     randomNumber = Math.floor(Math.random() * 3) + 1 ;
     let text;
     switch (randomNumber) {
         case 1:
-            text = "Rock";
+            text = "rock";
             break;
         case 2:
             text = "paper";
             break;
         case 3:
-            text ="Scissors";
+            text ="scissors";
             break;
     }         
     return text;
 }
 
+
+
 computerChoice = getComputerChoice();
 console.log(randomNumber)
 console.log(computerChoice)
+console.log(playerChoice)
+
