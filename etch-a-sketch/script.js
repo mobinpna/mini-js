aebf9bec9c554a01a07f0ceb8d513279c385f5d0
mobin <pnamobin@gmail.com>
2023-07-29 02:42:11 +0330

    Create the canvas tiles using for loop

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -4,6 +4,17 @@ const container = document.querySelector('.canvasContainer');
 
 for (let i = squareNum; i > 0 ;i--){
     let row = document.createElement('div');
-    row.classList.add('rowContainer')
+    row.classList.add(`rowContainer` );
+    row.classList.add(`row${i}`);
     container.appendChild(row);
 }  
+
+
+for (let i = squareNum; i > 0 ;i--){
+    let rowContainer = document.querySelector(`.row${i}`);
+  for(let j=squareNum; j > 0; j--){
+    let column =document.createElement('div');
+    column.classList.add('canvasTile');
+    rowContainer.appendChild(column);
+  }
+}
