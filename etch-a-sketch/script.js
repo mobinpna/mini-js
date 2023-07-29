956106a2fccaef6175deefe286c71a08627ccfd7
mobin <pnamobin@gmail.com>
2023-07-29 07:14:43 +0330

    Add clear and new canvas buttons and functions

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,35 +1,56 @@
-let squareNum;
-squareNum = 16;
+let squareNum = 16;
 const container = document.querySelector('.canvasContainer');
 
-for (let i = squareNum; i > 0 ;i--){
-    let row = document.createElement('div');
-    row.classList.add(`rowContainer` );
-    row.classList.add(`row${i}`);
-    container.appendChild(row);
-}  
+function createCanvas(){
+    const rmTiles = document.querySelectorAll('.rowContainer');
+    if(rmTiles.length !== 0){
+        rmTiles.forEach(row => row.parentNode.removeChild(row));
+    }
 
-for (let i = squareNum; i > 0 ;i--){
-    let rowContainer = document.querySelector(`.row${i}`);
-  for(let j=squareNum; j > 0; j--){
-    let column =document.createElement('div');
-    column.classList.add('canvasTile');
-    column.setAttribute("id", `column${j}row${i}`)
-    rowContainer.appendChild(column);
-  }
+    for (let i = squareNum; i > 0 ;i--){
+        let row = document.createElement('div');
+        row.classList.add(`rowContainer` );
+        row.classList.add(`row${i}`);
+        container.appendChild(row);
+    }  
+
+    for (let i = squareNum; i > 0 ;i--){
+        let rowContainer = document.querySelector(`.row${i}`);
+        for(let j=squareNum; j > 0; j--){
+            let column =document.createElement('div');
+            column.classList.add('canvasTile');
+            column.setAttribute("id", `column${j}row${i}`)
+            rowContainer.appendChild(column);
+        }
+    }
+    const canvasTiles = document.querySelectorAll('.canvasTile');
+    for (let i = canvasTiles.length-1; i>=0; i--) {
+        canvasTiles[i].addEventListener('mouseover', (e) => changeColor(e.target.id));
+    }
 }
 
-function ColorCodeGenrator(){
+function ColorCodeGenerator(){
     let colorCode = Math.floor(Math.random() * 256);
     return colorCode;
 }
 
 function changeColor(id) {
     let tile = document.querySelector(`#${id}`);
-    tile.setAttribute('style', `background-color: rgb(${ColorCodeGenrator()},${ColorCodeGenrator()},${ColorCodeGenrator()});`)
+    tile.setAttribute('style', `background-color: rgb(${ColorCodeGenerator()},${ColorCodeGenerator()},${ColorCodeGenerator()});`)
 }
 
-const canvasTiles = document.querySelectorAll('.canvasTile');
-for (let i = canvasTiles.length-1; i>=0; i--) {
-    canvasTiles[i].addEventListener('mouseover', (e) => changeColor(e.target.id));
+function clearCanvas() {
+    const tiles = document.querySelectorAll('.canvasTile');
+    tiles.forEach(tile => tile.style.backgroundColor = 'black');
 }
+
+createCanvas();
+
+const newCanvas= document.querySelector('.new');
+newCanvas.addEventListener('click', () => {
+    squareNum = prompt('How many squares per side?');
+    createCanvas();
+});
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => clearCanvas());
